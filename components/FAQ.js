9ad1804adf106a32924f44dbfdc14a5cbea27a31
kaumonaung <ag.kaumon@gmail.com>
2022-01-16 12:28:20 +0100

    Commented Disclosure from headlessui

--- components/FAQ.js
+++ components/FAQ.js
@@ -11,7 +11,7 @@ const FAQ = () => {
         </h2>
 
         <div className='max-w-lg mx-auto px-5'>
-          <Disclosure>
+          {/* <Disclosure>
             {({ open }) => (
               <>
                 <Disclosure.Button className='flex justify-between w-full px-4 py-2 text-base font-bold text-left text-rose-600 bg-slate-100 rounded-lg hover:bg-slate-200 focus:outline-none focus-visible:ring focus-visible:ring-rose-700'>
@@ -109,7 +109,7 @@ const FAQ = () => {
                 </Disclosure.Panel>
               </>
             )}
-          </Disclosure>
+          </Disclosure> */}
         </div>
       </div>
     </div>
