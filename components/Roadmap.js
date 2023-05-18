18754f6c80b891ea865b4392652c55e6d9bc785b
kaumonaung <47107420+kaumonaung@users.noreply.github.com>
2023-05-18 18:25:11 +0200

    Marked as portfolio project

--- components/Roadmap.js
+++ components/Roadmap.js
@@ -2,82 +2,74 @@ import React from 'react';
 import Image from 'next/image';
 import Ticket from '../public/assets/Golden_Tickets.png';
 
+const milestones = [
+  {
+    percentage: 10,
+    description:
+      'Tell you that CryptoKon is just a fictional site for a portfolio project.',
+  },
+  {
+    percentage: 30,
+    description:
+      'CryptoKons, held back from initial sale, will be airdropped to CryptoKon holders randomly.',
+  },
+  {
+    percentage: 50,
+    description: "Team will be expanded to realize CryptoKon's future.",
+  },
+  {
+    percentage: 80,
+    description:
+      'An online store exclusive for CryptoKon holders. Gain access to premium merch and collectibles.',
+  },
+  {
+    percentage: 100,
+    description:
+      'Participation and voting on the CryptoKon app development will be enabled for all CryptoKon holders. Get first access!',
+  },
+];
+
 const Roadmap = () => {
   return (
-    <div className='max-w-5xl mx-auto py-10'>
+    <div className="max-w-5xl mx-auto py-10">
       <div>
-        <h2 className='text-slate-100 italic font-bold text-4xl uppercase tracking-wider py-5 px-10'>
+        <h2 className="text-slate-100 italic font-bold text-4xl uppercase tracking-wider py-5 px-10">
           CryptoKon Roadmap
         </h2>
-        <p className='text-slate-400 max-w-2xl px-10'>
+        <p className="text-slate-400 max-w-2xl px-10">
           CryptoKon is not just an NFT art collection, it&apos;ll be the first
           game-like digital convention about crypto and blockchain.
         </p>
-        <p className='text-slate-400 max-w-2xl px-10 mt-2'>
+        <p className="text-slate-400 max-w-2xl px-10 mt-2">
           We&apos;ve set up some milestones for ourselves. Once we hit a target
           sell through percentage, we will start expanding and working on them.
         </p>
       </div>
 
-      <div className='md:flex flex-row items-center'>
-        <div className='grid grid-cols-2 max-w-5xl mx-auto py-16'>
-          <div className='grid grid-cols-8 gap-10 items-center col-span-2 px-10 pb-5'>
-            <div className='col-span-3 sm:col-span-2'>
-              <p className='bg-gradient-to-tl from-rose-500 to-pink-500 font-medium text-transparent bg-clip-text text-xl'>
-                10%
-              </p>
-            </div>
-            <div className='col-span-5 sm:col-span-6 text-slate-300'>
-              Quit our day jobs and work full time on this.
-            </div>
-
-            <div className='col-span-3 sm:col-span-2'>
-              <p className='bg-gradient-to-tl from-rose-500 to-pink-500 font-medium text-transparent bg-clip-text text-xl'>
-                30%
-              </p>
-            </div>
-            <div className='col-span-5 sm:col-span-6 text-slate-300'>
-              CryptoKons, held back from initial sale, will be airdropped to
-              CryptoKon holders randomly.
-            </div>
-
-            <div className='col-span-3 sm:col-span-2'>
-              <p className='bg-gradient-to-tl from-rose-500 to-pink-500 font-medium text-transparent bg-clip-text text-xl'>
-                50%
-              </p>
-            </div>
-            <div className='col-span-5 sm:col-span-6 text-slate-300'>
-              Team will be expanded to realize CryptoKon&apos;s future.
-            </div>
-
-            <div className='col-span-3 sm:col-span-2'>
-              <p className='bg-gradient-to-tl from-rose-500 to-pink-500 font-medium text-transparent bg-clip-text text-xl'>
-                80%
-              </p>
-            </div>
-            <div className='col-span-5 sm:col-span-6 text-slate-300'>
-              An online store exclusive for CryptoKon holders. Gain access to
-              premium merch and collectibles.
-            </div>
-
-            <div className='col-span-3 sm:col-span-2'>
-              <p className='bg-gradient-to-tl from-rose-500 to-pink-500 font-medium text-transparent bg-clip-text text-xl'>
-                100%
-              </p>
-            </div>
-            <div className='col-span-5 sm:col-span-6 text-slate-300'>
-              Participation and voting on the CryptoKon app development will be
-              enabled for all CryptoKon holders. Get first access!
-            </div>
+      <div className="md:flex flex-row items-center">
+        <div className="grid grid-cols-2 max-w-5xl mx-auto py-16">
+          <div className="grid grid-cols-8 gap-10 items-center col-span-2 px-10 pb-5">
+            {milestones.map((milestone) => (
+              <>
+                <div className="col-span-3 sm:col-span-2">
+                  <p className="bg-gradient-to-tl from-rose-500 to-pink-500 font-medium text-transparent bg-clip-text text-xl">
+                    {milestone.percentage}%
+                  </p>
+                </div>
+                <div className="col-span-5 sm:col-span-6 text-slate-300">
+                  {milestone.description}
+                </div>
+              </>
+            ))}
           </div>
         </div>
 
-        <div className='px-10 max-w-xs mx-auto'>
+        <div className="px-10 max-w-xs mx-auto">
           <Image
             src={Ticket}
-            alt='CryptoKon Rarity Specs'
+            alt="CryptoKon Rarity Specs"
             quality={100}
-            className='text-white rounded'
+            className="text-white rounded"
           />
         </div>
       </div>
--- components/WelcomeSection.js
+++ components/WelcomeSection.js
@@ -7,53 +7,54 @@ import NFT4 from '../public/assets/CryptoKon_4.png';
 
 const WelcomeSection = () => {
   return (
-    <div className='max-w-5xl mx-auto my-16 px-10 md:flex flex-row gap-10 items-center'>
-      <div className='md:basis-1/2'>
-        <h1 className='text-slate-100 font-bold italic text-3xl uppercase tracking-wider py-3'>
+    <div className="max-w-5xl mx-auto my-16 px-10 md:flex flex-row gap-10 items-center">
+      <div className="md:basis-1/2">
+        <h1 className="text-slate-100 font-bold italic text-3xl uppercase tracking-wider py-3">
           Welcome to CryptoKon
         </h1>
-        <p className='text-slate-400'>
+        <p className="text-slate-400">
           CryptoKon will be an exclusive digital convention with everything
           about crypto and blockchain. Yet, it will feel and play like a RPG
-          where you control your own character, i. e. your NFT
+          where you control your own character, i. e. your NFT (reminder that
+          this is a portfolio project).
         </p>
         <br />
-        <p className='text-slate-400'>
+        <p className="text-slate-400">
           Each NFT in our collection is a ticket but also your character. Only
           CryptoKon holders can take part as visitors.
         </p>
         <br />
-        <p className='text-slate-400'>
+        <p className="text-slate-400">
           It&apos;s not finished yet and we need your help. To create the best
           game-like, pure digital congress, we need your support. Join us on
           Discord or mint one of our NFTs on launch day.
         </p>
       </div>
 
-      <div className='md:basis-1/2 grid grid-cols-2 sm:grid-cols-4 md:grid-cols-2 gap-4 py-10 lg:px-5'>
+      <div className="md:basis-1/2 grid grid-cols-2 sm:grid-cols-4 md:grid-cols-2 gap-4 py-10 lg:px-5">
         <Image
           src={NFT1}
-          alt='CryptoKon NFT #1'
+          alt="CryptoKon NFT #1"
           quality={100}
-          className='text-white rounded'
+          className="text-white rounded"
         />
         <Image
           src={NFT2}
-          alt='CryptoKon NFT #2'
+          alt="CryptoKon NFT #2"
           quality={100}
-          className='text-white rounded'
+          className="text-white rounded"
         />
         <Image
           src={NFT3}
-          alt='CryptoKon NFT #3'
+          alt="CryptoKon NFT #3"
           quality={100}
-          className='text-white rounded'
+          className="text-white rounded"
         />
         <Image
           src={NFT4}
-          alt='CryptoKon NFT #4'
+          alt="CryptoKon NFT #4"
           quality={100}
-          className='text-white rounded'
+          className="text-white rounded"
         />
       </div>
     </div>
